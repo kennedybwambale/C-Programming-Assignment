59ea2400758c79510ef6c9929cb34434cc46a2db
kennedybwambale <38218260+kennedybwambale@users.noreply.github.com>
2018-04-11 10:52:08 -0400

    Question one
    Correcting the error in the code

---
+++ assignment/question 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ int x=1;//the termination used was wrong. Right one was ;
+
+int main()
+{ if(x=1)
+   {
+
+    printf(" x equals 1");
+   }else{//not otherwise
+       printf(" x does not equal 1");
+
+   }
+    return 0;
+}
